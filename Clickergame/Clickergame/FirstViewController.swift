05cef7096dbb684a00f7ad072977b14ef15c838c
Will Yelton <willyelton@steveny-macbook.local>
2017-07-05 19:34:41 -0400

    Added base xcode files

---
+++ Clickergame/Clickergame/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Clickergame
+//
+//  Created by Will Yelton on 7/5/17.
+//  Copyright © 2017 Parenthium. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
